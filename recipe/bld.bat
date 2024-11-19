6dc160a77140613c4e2a0ffc9e0993a4aa608744
Tomasz Benes <tbenes@anaconda.com>
2024-11-19 12:16:48 +0000

    8.4.0

---
+++ recipe/bld.bat
@@ -0,0 +1,11 @@
+@echo on
+
+set "PROTOBUF_INCLUDE_DIR=%LIBRARY_INC%"
+set "PROTOBUF_LIB_DIR=%LIBRARY_LIB%"
+set "PROTOC=%LIBRARY_BIN%/protoc.exe"
+set "MYSQL_CAPI=%LIBRARY_PREFIX%"
+set "EXTRA_COMPILE_ARGS=/DPROTOBUF_USE_DLLS"
+set "SKIP_VENDOR=1"
+
+cd mysql-connector-python
+%PYTHON% -m pip install . --no-deps --verbose --no-build-isolation
