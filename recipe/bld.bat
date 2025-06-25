f98d3f5526f48a01f4b4e0fbbda3736414b11419
Tomasz Beneś <tbenes@anaconda.com>
2025-06-25 16:36:19 +0200

    re-add --no-build-isolation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,4 +8,4 @@ set "EXTRA_COMPILE_ARGS=/DPROTOBUF_USE_DLLS"
 set "SKIP_VENDOR=1"
 
 cd mysql-connector-python
-%PYTHON% -m pip install . --no-deps --verbose
+%PYTHON% -m pip install . --no-deps --verbose --no-build-isolation
